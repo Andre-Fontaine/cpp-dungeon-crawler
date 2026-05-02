9dc76e796f9dcccda3a9e8dc97620cf41ae4a538
Andre Fontaine <andre@andrefontaine.ca>
2026-05-02 19:09:22 -0400

    Add Rooms and Items class with inheritance. Add GameStart function.  Add inheritances to Enemy class.

--- source.cpp
+++ source.cpp
@@ -2,6 +2,12 @@
 #include "source.h"
 using namespace std;
 
+void GameStart()
+{
+	cout << "Welcome to Dungeon Crawler!" << endl;
+	cout << "You will play as Andre, a lost soul waking up in an unknown dungeon." << endl;
+	cout << "You won't know what lies ahead through this labyrinth. Find the means to survive." << endl << endl;
+}
 class Player
 {
 public:
@@ -27,6 +33,16 @@ public:
 	}
 };
 
+class Rooms
+{
+public:
+	string name;
+	string description;
+	bool hasEnemy;
+	bool hasItem;
+};
+
+
 class Enemy : public Player
 {
 public:
@@ -36,19 +52,85 @@ public:
 	}
 };
 
+class Items
+{
+public:
+	string name;
+	string description;
+	float damageBonus;
+
+	Items()
+	{
+		damageBonus = 0;
+	}
+
+	void UseItem(Player& player)
+	{
+		int totalDamage = player.baseDamage + damageBonus;
+		player.baseDamage += damageBonus;
+		cout << player.name << " picks up a " << name << " and gains " << damageBonus << " bonus damage, totalling " << totalDamage << " damage!" << endl;
+	}
+};
+
+void EnterRoom(const Rooms& rooms)
+{
+	cout << rooms.description << endl;
+}
+
+void EncounterEnemy(const Enemy& enemy)
+{
+	cout << endl 
+		<< "You encountered: " << enemy.name << endl
+		<< "Health: " << enemy.health << endl
+		<< "Damage: " << enemy.baseDamage << endl
+		<< endl;
+}
+
 int main()
 {
+	GameStart();
+
 	//Creating player character with stats
 	Player Andre;
 	Andre.name = "Andre";
 	Andre.health = 100;
 	Andre.baseDamage = 10;
 
-	//Creating enemy with stats
+	//Creating enemy Spider with stats
 	Enemy Spider;
 	Spider.name = "Spider";
 	Spider.health = 50;
 	Spider.baseDamage = 5;
 
-	Andre.TakeDamage(Spider.baseDamage);
+	//Creating enemy Giant Rat with stats
+	Enemy GiantRat;
+	GiantRat.name = "Giant Rat";
+	GiantRat.health = 75;
+	GiantRat.baseDamage = 10;
+
+	//Creating enemy Goblin with stats
+	Enemy Goblin;
+	Goblin.name = "Goblin";
+	Goblin.health = 100;
+	Goblin.baseDamage = 20;
+
+	//Creating item Sword with stats
+	Items Sword;
+	Sword.name = "Sword";
+	Sword.description = "A rusty sword that hasn't been used in ages.";
+	Sword.damageBonus = 15;
+
+	//Creating a room with description and properties
+	Rooms Cellar;
+	Cellar.name = "Cellar";
+	Cellar.description = "The room is dark and damp. There's dusty kegs, cleaning supplies, and empty glasses everywhere. This place hasn't been occupied in what seems to be decades. Right ahead is a door. Behind me is a stairwell going down. Which should I take?";
+	Cellar.hasEnemy = false;
+	Cellar.hasItem = false;
+
+	//Simulating combat
+	EnterRoom(Cellar);
+	EncounterEnemy(Goblin);
+		Sword.UseItem(Andre);
+		cout << Andre.name << " attacks " << Goblin.name << " for " << Andre.baseDamage << " damage!" << endl;
+		Goblin.TakeDamage(Andre.baseDamage);
 }
--- source.h
+++ source.h
@@ -3,3 +3,11 @@
 class Player;
 
 class Enemy;
+
+class Items;
+
+class Rooms;
+
+void EncounterEnemy(const Enemy& enemy);
+
+void GameStart();
